191b046f39ebe1ff1401d357ec76e0b341ad2917
Farshid <Shekari.farshid@gmail.com>
2018-01-26 17:40:43 +0330

    Project main File.

--- Main.py
+++ Main.py
@@ -1,37 +1,38 @@
-#https://arxiv.org/pdf/0709.2938.pdf
+# https://arxiv.org/pdf/0709.2938.pdf
 
 from collections import Counter
 
 
-def loadData():
-    graph={}
-    dict={}
+def initialize():
+    graph = {}
+    dict = {}
     with open("YpInteraction.txt") as f:
         for line in f:
-            dict.update({line.strip('\n').split()[0]:line.strip('\n').split()[1]})
-            dict.update({line.strip('\n').split()[1]:line.strip('\n').split()[0]})
-            #graph.append((line.split()[0], line.split()[1]))
-    #graph.sort(key=lambda x: x[0])
-    #print(graph)
+            dict.update({line.strip('\n').split()[0]: 0})
+            dict.update({line.strip('\n').split()[1]: 0})
+            # graph.append((line.split()[0], line.split()[1]))
+    # graph.sort(key=lambda x: x[0])
+    # print(graph)
 
     # https://stackoverflow.com/questions/16013485/counting-the-amount-of-occurrences-in-a-list-of-tuples
-    #print(Counter(elem[0] for elem in graph))
-    #print(Counter(elem[1] for elem in graph))
-    print(dict,len(dict))
+    # print(Counter(elem[0] for elem in graph))
+    # print(Counter(elem[1] for elem in graph))
+    print(dict, len(dict))
     return graph
-def initialize():
-    return "Order"
+
 
 def arrangeNodesOrder():
     return
 
+
 def labelPropagate():
     return
 
+
 if __name__ == '__main__':
-    #print('Hi')
-    loadData()
+    # print('Hi')
+    # loadData()
     initialize()
-    t=1
+    t = 1
     arrangeNodesOrder()
     labelPropagate()
