103ea5c1b58e7f098c94bddc2d1828d7cec92b6e
Farshid <Shekari.farshid@gmail.com>
2018-01-26 19:26:35 +0330

    Project main File.

--- Main.py
+++ Main.py
@@ -1,20 +1,24 @@
 # https://arxiv.org/pdf/0709.2938.pdf
 
 from collections import Counter
+from random import shuffle
 
 
 def initialize():
+    rndOrder = []
     graph = []
     dict = {}
     with open("YpInteraction.txt") as f:
         for line in f:
             dict.update({line.strip('\n').split()[0]: 0})
             dict.update({line.strip('\n').split()[1]: 0})
+            graph.append([line.strip('\n').split()[0], line.strip('\n').split()[1]])
     lbl = 0
     for item in dict.items():
-        graph.append([item[0], [lbl]])
+        rndOrder.append([item[0], [lbl]])
         lbl += 1
-    return graph
+    shuffle(rndOrder)
+    return rndOrder,graph
 
 
 def arrangeNodesOrder():
@@ -26,7 +30,11 @@ def labelPropagate():
 
 
 if __name__ == '__main__':
-    initialize()
+    # step1
+    graph = initialize()
+
     t = 1
+
     arrangeNodesOrder()
+
     labelPropagate()
